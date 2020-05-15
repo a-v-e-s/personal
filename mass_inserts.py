b60842c1981eb02c5ab01357ed3214ba7fd2e963
Jon David Tannehill <jdtannehill2490@gmail.com>
2020-05-15 15:43:59 -0500

    mass_inserts.parser() works well now.

--- mass_inserts.py
+++ mass_inserts.py
@@ -1,4 +1,5 @@
 import re, sqlite3
+import datetime
 
 
 patt = re.compile(r'^\d{1,2}\/\d{1,2}\/\d{1,2}')
@@ -9,11 +10,24 @@ def parse(text):
     db = sqlite3.Connection('me.db')
     curs = db.cursor()
     #
+    statement1 = "INSERT INTO Daily_Functionality (Weekday, Date, Project_Hours, Study_Hours, Administrative_Hours, Total_Work, Strength_Training, Cardio, Mobility, Total_Exercise, Social_Hours, Sleep_1, Sleep_2, Total_Replenishing, Burnout, Quality_Of_Life) Values ("
+    statement2 = "INSERT INTO Daily_Functionality (Weekday, Date, Project_Hours, Study_Hours, Administrative_Hours, Total_Work, Strength_Training, Cardio, Mobility, Total_Exercise, Social_Hours, Sleep_1, Sleep_2, Total_Replenishing, Indulging, Flaking, Cursing, Intoxication, Total_Vices, Burnout, Quality_Of_Life) Values ("
+    #
     count = 0
     for x in text.split('\n'):
         if count >= 7:
             break
-        statement = "INSERT INTO Daily_Functionality (Weekday, Date, Project_Hours, Study_Hours, Administrative_Hours, Total_Work, Strength_Training, Cardio, Mobility, Total_Exercise, Social_Hours, Sleep_1, Sleep_2, Total_Replenishing, Indulging, Flaking, Cursing, Intoxication, Total_Vices, Burnout, Over_Extending, Quality_Of_Life) Values ("
+        for y in x.split('\t'):
+            y = y.strip(' ')
+            if re.match(patt, y):
+                vals = y.split('/')
+                vals[2] = '20' + vals[2]
+                vals = [int(z.lstrip('0')) for z in vals]
+                if datetime.date(vals[2], vals[0], vals[1]) < datetime.date(2019, 9, 29):
+                    statement = statement1
+                else:
+                    statement = statement2
+                break
         for y in x.split('\t'):
             y = y.strip(' ')
             if re.match(patt, y):
@@ -30,18 +44,26 @@ def parse(text):
             statement += y + ', '
         statement = re.sub(r', $', r'', statement)
         statement += ');'
-        print(statement, end='\n\n\n')
-        #curs.execute(statement)
+        #
+        #print(statement, end='\n\n\n')
+        try:
+            curs.execute(statement)
+        except Exception as e:
+            print(e)
+            break
+        #
         count += 1
     #
     db.commit()
+    curs.close(); db.close()
 
 
 def compile_weekly():
     db = sqlite3.Connection('me.db')
     curs = db.cursor()
     #
-    statement = "INSERT INTO Weekly_Functionality (Starting_Date, Ending_Date, Total_Project_Hours, Total_Study_Hours, Total_Administrative_Hours, Total_Work, Total_Strength_Training, Total_Cardio, Total_Mobility, Total_Exercise, Total_Social_Hours, Total_Sleep_1, Total_Sleep_2, Total_Replenishing, Total_Indulging, Total_Flaking, Total_Cursing, Total_Intoxication, Total_Vices, Total_Burnout, Total_Over_Extending, Total_Quality_Of_Life) Values ("
+    statement1 = "INSERT INTO Weekly_Functionality (Starting_Date, Ending_Date, Total_Project_Hours, Total_Study_Hours, Total_Administrative_Hours, Total_Work, Total_Strength_Training, Total_Cardio, Total_Mobility, Total_Exercise, Total_Social_Hours, Total_Sleep_1, Total_Sleep_2, Total_Replenishing, Total_Burnout, Total_Over_Extending, Total_Quality_Of_Life) Values ("
+    statement2 = "INSERT INTO Weekly_Functionality (Starting_Date, Ending_Date, Total_Project_Hours, Total_Study_Hours, Total_Administrative_Hours, Total_Work, Total_Strength_Training, Total_Cardio, Total_Mobility, Total_Exercise, Total_Social_Hours, Total_Sleep_1, Total_Sleep_2, Total_Replenishing, Total_Indulging, Total_Flaking, Total_Cursing, Total_Intoxication, Total_Vices, Total_Burnout, Total_Over_Extending, Total_Quality_Of_Life) Values ("
     #
     # lots of stuff
     #
